0331d040e7ff7b96385651a5b25bfe432cf25047
Casey Schaufler <casey@schaufler-ca.com>
2025-05-16 12:23:14 -0700

    UBUNTU: SAUCE: apparmor5.0.0 [2/53]: Stacking: LSM: Exclusive secmark usage
    BugLink: https://bugs.launchpad.net/bugs/2121552
    
    The network secmark can only be used by one security module
    at a time. Establish mechanism to identify to security modules
    whether they have access to the secmark. SELinux already
    incorparates mechanism, but it has to be added to Smack and
    AppArmor.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -1513,7 +1513,7 @@ static int apparmor_socket_sock_rcv_skb(struct sock *sk, struct sk_buff *skb)
 	struct aa_sk_ctx *ctx = aa_sock(sk);
 	int error;
 
-	if (!skb->secmark)
+	if (!aa_secmark() || !skb->secmark)
 		return 0;
 
 	/*
@@ -1642,7 +1642,7 @@ static int apparmor_inet_conn_request(const struct sock *sk, struct sk_buff *skb
 	struct aa_sk_ctx *ctx = aa_sock(sk);
 	int error;
 
-	if (!skb->secmark)
+	if (!aa_secmark() || !skb->secmark)
 		return 0;
 
 	rcu_read_lock();
@@ -1662,6 +1662,7 @@ struct lsm_blob_sizes apparmor_blob_sizes __ro_after_init = {
 	.lbs_file = sizeof(struct aa_file_ctx),
 	.lbs_task = sizeof(struct aa_task_ctx),
 	.lbs_sock = sizeof(struct aa_sk_ctx),
+	.lbs_secmark = true,
 };
 
 static const struct lsm_id apparmor_lsmid = {
@@ -2361,7 +2362,7 @@ static unsigned int apparmor_ip_postroute(void *priv,
 	struct sock *sk;
 	int error;
 
-	if (!skb->secmark)
+	if (!aa_secmark() || !skb->secmark)
 		return NF_ACCEPT;
 
 	sk = skb_to_full_sk(skb);
